78472ec1796cf6178028512eac30965e4328244c
balayaydemir <balayaydemir@gmail.com>
2019-10-11 10:30:32 -0700

    final final

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ import './App.css';
 // This object will allow us to
 // easily convert numbers into US dollar values
 
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
 class App extends Component {
   state = {
     selected: {
